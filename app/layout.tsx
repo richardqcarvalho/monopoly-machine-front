5e5bd48ad780ab12472d009e5b7faa4bf1e45c04
Richard Carvalho <richardcarvalhooo@gmail.com>
2024-12-20 18:19:13 -0300

    feature: as it is a mobile app, not displaying it if it is a computer screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${roboto.className} antialiased`}>{children}</body>
+      <body
+        className={`${roboto.className} flex h-dvh w-screen bg-blue-900 antialiased`}
+      >
+        <div className='hidden h-full w-full items-center justify-center sm:flex'>
+          <h1 className='text-2xl font-bold text-white'>
+            Mobile-only application
+          </h1>
+        </div>
+        <div className='flex w-full sm:hidden'>{children}</div>
+      </body>
     </html>
   )
 }
