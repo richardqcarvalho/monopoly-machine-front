23b1bbaf9d4505f8ce8d04aa59bc9e1084ab1e82
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-02-04 10:37:37 -0300

    fix: preventing create account button to fire login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -29,55 +29,57 @@ const Login = () => {
   }
 
   return (
-    <div className='flex h-screen w-screen items-center justify-center gap-10 bg-blue-900'>
-      <form
-        onSubmit={onSubmit}
-        className='flex flex-col gap-6'
-      >
-        <div className='relative'>
-          <label
-            htmlFor='name'
-            className='absolute -top-2.5 left-3 bg-blue-900 p-1 text-xs text-white'
-          >
-            Name
-          </label>
-          <input
-            id='name'
-            className='w-64 rounded-md border border-white bg-blue-900 px-4 py-3 text-base text-white outline-none placeholder:text-white/50'
-            placeholder='Type your name'
-            onChange={e => setName(e.target.value)}
-            value={name}
-          />
-        </div>
-        <div className='relative'>
-          <label
-            htmlFor='password'
-            className='absolute -top-2.5 left-3 bg-blue-900 p-1 text-xs text-white'
-          >
-            Password
-          </label>
-          <input
-            id='password'
-            className='w-64 rounded-md border border-white bg-blue-900 px-4 py-3 text-base text-white outline-none placeholder:text-white/50'
-            placeholder='Type your password'
-            onChange={e => setPassword(e.target.value)}
-            value={password}
-            type='password'
-          />
-        </div>
-        <button
-          type='submit'
-          className='cursor-pointer rounded-md border border-white bg-blue-900 px-4 py-3 text-base text-white outline-none'
+    <div className='flex h-screen w-screen flex-col items-center justify-center gap-10 bg-blue-900'>
+      <div className='flex flex-col gap-6'>
+        <form
+          onSubmit={onSubmit}
+          className='flex flex-col gap-6'
         >
-          Enter
-        </button>
+          <div className='relative'>
+            <label
+              htmlFor='name'
+              className='absolute -top-2.5 left-3 bg-blue-900 p-1 text-xs text-white'
+            >
+              Name
+            </label>
+            <input
+              id='name'
+              className='w-64 rounded-md border border-white bg-blue-900 px-4 py-3 text-base text-white outline-none placeholder:text-white/50'
+              placeholder='Type your name'
+              onChange={e => setName(e.target.value)}
+              value={name}
+            />
+          </div>
+          <div className='relative'>
+            <label
+              htmlFor='password'
+              className='absolute -top-2.5 left-3 bg-blue-900 p-1 text-xs text-white'
+            >
+              Password
+            </label>
+            <input
+              id='password'
+              className='w-64 rounded-md border border-white bg-blue-900 px-4 py-3 text-base text-white outline-none placeholder:text-white/50'
+              placeholder='Type your password'
+              onChange={e => setPassword(e.target.value)}
+              value={password}
+              type='password'
+            />
+          </div>
+          <button
+            type='submit'
+            className='cursor-pointer rounded-md border border-white bg-blue-900 px-4 py-3 text-base text-white outline-none'
+          >
+            Enter
+          </button>
+        </form>
         <button
           onClick={goToCreateAccount}
           className='cursor-pointer rounded-md border border-white bg-blue-900 px-4 py-3 text-base text-white outline-none'
         >
           Create account
         </button>
-      </form>
+      </div>
     </div>
   )
 }
