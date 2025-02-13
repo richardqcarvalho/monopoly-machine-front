e4155e23200300c62114c426585cc0af177c9dcc
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-02-13 16:53:15 -0300

    feat: adding theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import { Router } from '@/routes'
 import '@/styles/index.css'
 import { createRoot } from 'react-dom/client'
 
 const root = document.getElementById('root') as HTMLElement
 
-createRoot(root).render(<Router />)
+createRoot(root).render(
+  <ThemeProvider>
+    <Router />
+  </ThemeProvider>,
+)
