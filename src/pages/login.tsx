316c8124a01377934dae44d8319436b7acce1d57
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-02-17 12:09:08 -0300

    style: restyling login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,15 @@
 import { login } from '@/actions/player'
 import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
 import { LoginT } from '@/types/player'
 import { useMutation } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
@@ -20,37 +29,58 @@ export const Login = () => {
   })
 
   return (
-    <div className='flex h-screen w-screen flex-col items-center justify-center gap-10'>
-      <div className='flex flex-col gap-6'>
+    <Card className='w-80'>
+      <CardHeader>
+        <CardTitle className='text-2xl'>Login</CardTitle>
+        <CardDescription>Login with your name and password</CardDescription>
+      </CardHeader>
+      <CardContent>
         <form
           onSubmit={handleSubmit(fields => mutate(fields))}
           className='flex flex-col gap-6'
         >
-          <Input
-            placeholder='Type your name'
-            {...register('name', { required: true })}
-          />
-          <Input
-            placeholder='Type your password'
-            type='password'
-            {...register('password', { required: true })}
-          />
-          <Button
-            type='submit'
-            disabled={isPending}
-            variant='outline'
-          >
-            Enter
-          </Button>
+          <div className='flex flex-col gap-2'>
+            <Label htmlFor='name'>Name</Label>
+            <Input
+              id='name'
+              placeholder='Type your name'
+              {...register('name', { required: true })}
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <Label htmlFor='password'>Password</Label>
+            <Input
+              id='password'
+              placeholder='Type your password'
+              type='password'
+              {...register('password', { required: true })}
+            />
+          </div>
+          <div>
+            <Button
+              type='submit'
+              disabled={isPending}
+              variant='outline'
+              className='w-full'
+            >
+              Enter
+            </Button>
+          </div>
         </form>
+        <div className='my-4 flex items-center gap-4'>
+          <Separator className='flex-1' />
+          <span>or</span>
+          <Separator className='flex-1' />
+        </div>
         <Button
           onClick={() => navigate('/create-account')}
           disabled={isPending}
           variant='outline'
+          className='w-full'
         >
           Create account
         </Button>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   )
 }
